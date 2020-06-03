39dbcc30579f0fe7a790d5a64e1225cfbb7265aa
Mfrnds <60433467+Mfrnds@users.noreply.github.com>
2020-06-03 12:26:57 +0200

    Changement connexionBdd

--- Site-PHP/connexionBdd.php
+++ Site-PHP/connexionBdd.php
@@ -1,15 +1,14 @@
 <?php
-/* Connexion au serveur et à la base de données */
 $host="sqletud.u-pem.fr";
-$user="yastier";
-$pwd="uGbeeuay58";
-$db="yastier_db";
-    // Connexion avec avec PDO
+$user="yastier"; /* Votre login */
+$pwd="uGbeeuay58"; /* Votre mot de passe */
+$db="yastier_db"; /* Le nom de votre base : de la forme ici : xxx_db avec xxx votre login */
+// Connexion avec avec PDO
 try{
-$con='mysql:host='.$host.';dbname='.$db;
-$dbh = new PDO($con,$user,$pwd,array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION)); }
-catch(Exception $e){
-die('Connexion impossible à la base de données !'.$e->getMessage());
+    $con='mysql:host='.$host.';dbname='.$db;
+    $dbh = new PDO($con,$user,$pwd,array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 }
-
+catch(Exception $e){
+    die('Connexion impossible à la base de données !'.$e->getMessage());
+}
  ?>
