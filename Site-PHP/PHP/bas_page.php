5ab5881cad5cef41c127e9e266eac194b67afade
Mfrnds <60433467+Mfrnds@users.noreply.github.com>
2020-06-11 15:43:45 +0200

    Updates

--- Site-PHP/PHP/bas_page.php
+++ Site-PHP/PHP/bas_page.php
@@ -1,4 +1,4 @@
-<?php include $_SERVER['PHP_SELF'];  ?>
+<?php echo basename($_SERVER['PHP_SELF']); ?>
 		<div class="baspage" style="bottom: <?php echo $bas; ?>">
 				<img src="image/upem.png" style="object-fit: contain; width: 50%;">
 				<img src="image/logo.png" style="object-fit: contain; width: 50%; right:0%;">
