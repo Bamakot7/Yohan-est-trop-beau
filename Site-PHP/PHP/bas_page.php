77b4d9ba8380cc761fae9498d3cd9ebd7660bb7e
Mfrnds <60433467+Mfrnds@users.noreply.github.com>
2020-06-11 18:10:53 +0200

    Test

--- Site-PHP/PHP/bas_page.php
+++ Site-PHP/PHP/bas_page.php
@@ -1,4 +1,3 @@
-<?php echo basename($_SERVER['PHP_SELF']); ?>
 		<div class="baspage" style="bottom: <?php echo $bas; ?>">
 				<img src="image/upem.png" style="object-fit: contain; width: 50%;">
 				<img src="image/logo.png" style="object-fit: contain; width: 50%; right:0%;">
