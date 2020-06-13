55a7e318a1f3728f52c56ec37d4393f8c233747c
Bamakot7 <54809551+Bamakot7@users.noreply.github.com>
2020-06-13 19:33:52 +0200

    message fr

--- Site-PHP/PHP/message_fr.php
+++ Site-PHP/PHP/message_fr.php
@@ -1,24 +1,36 @@
-<?php
-    session_start();
-?>
 
 <div class="messageTable">
     <table>
         <tr>
             <td>Auteur</td><td>Date</td><td>Contenu</td>
         </tr>
-    </table>
+        <?php
+            require_once("connexionBdd.php");
 
-    <table>
-        <form action="message.php" method="post">
-            <tr>
-                <td>Votre message</td><td>Envoyer</td>
-            </tr>
-            <tr>
-                <td><textarea name="message" rows="4" cols="50"></textarea></td><td><button type="submit">Envoyer</button></td>
-            </tr>
-        </form>
+            $requete = "SELECT * FROM `user` JOIN `commentaire` ON user.num = commentaire.numUser";
+            $data = $dbh->prepare($requete);
+            $data->execute();
+            $result = $data->fetchAll(\PDO::FETCH_ASSOC);
+
+            foreach($result as $row){
+               echo "<tr><td>".$row["Nom"]."</td><td>".$row["date"]."</td><td>".$row["commentaire"]."</td></tr>";
+            }
+            $data->closeCursor();
+        ?>
     </table>
+    
+    <form action="" method="post">
+        <table>
+            
+                <tr>
+                    <td>Votre message</td><td>Envoyer</td>
+                </tr>
+                <tr>
+                    <td><textarea name="msg" rows="4" cols="30" placeholder="Votre message ici!"></textarea></td>
+                    <td><button type="submit" name="Envoyer">Envoyer</button></td>
+                </tr>
+        </table>
+    </form>
 
 
 </div>
--- Site-PHP/message.php
+++ Site-PHP/message.php
@@ -1,10 +1,55 @@
 <?php 
     session_start();
+    
+    //si il n'y a pas de session, l'utilisateur retourne à l'accueil
+    if(!(isset($_SESSION["mail"]))){
+        header("location:accueil.php");
+    }
+    //si un message valide a été écrit on le publie
+    if(isset($_POST["msg"])){
+        
+        require_once("connexionBdd.php");
+        //on recup le numero d'utilisateur
+
+        $query = 'SELECT * FROM `user` WHERE mail like :mail AND MotDePasse like :mdp';
+        $statement = $dbh->prepare($query);
+        $statement->execute(  
+                array(  
+                    'mail'       =>     $_SESSION['mail'],
+                    'mdp'        =>     $_SESSION['mdp'],
+                )
+            );
+
+        $ligne = $statement->fetch(); // ligne du résultat
+
+        $msg = htmlentities($_POST["msg"]);
+        $numUser = $ligne['num'];
+        $dat = date("Y-m-d");
+        
+
+        
+
+        if ($msg != '') {
+
+            $query = 'INSERT INTO commentaire (commentaire, numUser, date) VALUES (:msg, :numUser, :dat)';
+            
+            $statement = $dbh->prepare($query);
+            $statement->execute(  
+                array(  
+                    'msg'        =>     $msg,
+                    'numUser'    =>     $numUser,
+                    'dat'        =>     $dat,
+                )
+            );
+        }
+    }
     require('PHP/entete_co.php');
+
     if ($_GET['lang'] == 'fr') {
         require('PHP/message_fr.php');
     } else {
         require('PHP/message_en.php');
     }
+
+
 ?>
-    
