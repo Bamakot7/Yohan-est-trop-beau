5b6cad26b21ab5623e8c537ecf6a7f2d67f60f94
Bamakot7 <54809551+Bamakot7@users.noreply.github.com>
2020-06-13 20:04:25 +0200

    message angais

--- Site-PHP/PHP/message_en.php
+++ Site-PHP/PHP/message_en.php
@@ -1,1 +1,37 @@
-pudding
+
+<div class="messageTable">
+    <table>
+        <tr>
+            <td>Author</td><td>Date</td><td>Message</td>
+        </tr>
+        <?php
+            require_once("connexionBdd.php");
+
+            $requete = "SELECT * FROM `user` JOIN `commentaire` ON user.num = commentaire.numUser";
+            $data = $dbh->prepare($requete);
+            $data->execute();
+            $result = $data->fetchAll(\PDO::FETCH_ASSOC);
+
+            foreach($result as $row){
+               echo "<tr><td>".$row["Nom"]." ".$row["Prenom"]."</td><td>".$row["date"]."</td><td>".$row["commentaire"]."</td></tr>";
+            }
+            $data->closeCursor();
+        ?>
+    </table>
+    
+    <form action="" method="post">
+        <table>
+            
+                <tr>
+                    <td>Your message</td><td>Send</td>
+                </tr>
+                <tr>
+                    <td><textarea name="msg" rows="4" cols="30" placeholder="Your message here!"></textarea></td>
+                    <td><button type="submit" name="Envoyer">Send</button></td>
+                </tr>
+        </table>
+    </form>
+
+
+</div>
+       
