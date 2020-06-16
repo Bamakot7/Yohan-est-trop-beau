3c93f7fa60fe66f60b5bda5277f0c2b976418288
Bamakot7 <54809551+Bamakot7@users.noreply.github.com>
2020-06-16 15:32:32 +0200

    Test pied de page

--- Site-PHP/PHP/bas_page.php
+++ Site-PHP/PHP/bas_page.php
@@ -1,4 +1,4 @@
-		<div class="baspage" style="bottom: <?php echo $bas; ?>">
+		<div class="baspage" style="bottom: 0;">
 				<img src="image/upem.png" style="object-fit: contain; width: 50%;">
 				<img src="image/logo.png" style="object-fit: contain; width: 50%; right:0%;">
 		</div>
